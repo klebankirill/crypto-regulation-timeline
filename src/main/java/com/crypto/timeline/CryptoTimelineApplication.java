f358207065c06a78b10f59c6be8038bfbc39a3bb
klebankirill <klebankira@gmail.com>
2026-02-28 14:57:55 +0800

    Migrate backend implementation from FastAPI to Spring Boot

---
+++ src/main/java/com/crypto/timeline/CryptoTimelineApplication.java
@@ -0,0 +1,11 @@
+package com.crypto.timeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoTimelineApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoTimelineApplication.class, args);
+    }
+}
